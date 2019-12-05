645b99cc74201bab85c10db82d7152d3f9ee986c
william-vega <willial1393@gmail.com>
2019-12-05 18:52:56 -0500

    config script service

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const Service = require('node-windows').Service;
 const svc = new Service({
-    name: 'lachamelia-api',
+    name: 'lachamelia_api',
     description: 'La chamelia api port ' + process.env.PORT,
     script: __dirname + '\\build\\app.js'
 });
@@ -12,8 +12,13 @@ svc.on('uninstall', function () {
 });
 
 svc.on('install', function () {
-    console.log('Listen on port ' + process.env.PORT);
-    svc.start();
+    try {
+        svc.start();
+        console.log('state', svc);
+        console.log('Listen on port ' + process.env.PORT);
+    } catch (e) {
+        console.log('error', e);
+    }
 });
 
 svc.install();
