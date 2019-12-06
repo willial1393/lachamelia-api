ce9a05c26300528fb65f42950669f57e5695bbeb
william-vega <willial1393@gmail.com>
2019-12-05 19:20:33 -0500

    config script service

--- server.js
+++ server.js
@@ -7,22 +7,24 @@ const svc = new Service({
 });
 
 svc.on('uninstall', function () {
-    console.log('Uninstall complete.');
-    console.log('The service exists: ', svc.exists);
+    console.log('Uninstall complete', !svc.exists);
     svc.install();
 });
 
 svc.on('install', function () {
     try {
-        console.log('The service exists: ', svc.exists);
-        console.log('Listen on port ' + process.env.PORT);
+        console.log('install complete', svc.exists);
         svc.start();
+        if (svc.exists) {
+            console.log('Listen on port ' + process.env.PORT);
+        }
     } catch (e) {
         console.log(e);
     }
 });
 
 if (svc.exists) {
+    svc.stop();
     svc.uninstall();
 } else {
     svc.install();
