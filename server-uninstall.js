1c36288575240a04fb0ee0336a27a4ad44d25228
william-vega <willial1393@gmail.com>
2019-12-06 01:28:45 -0500

    config script service

--- server-uninstall.js
+++ server-uninstall.js
@@ -12,17 +12,6 @@ svc.on('stop', function () {
 
 svc.on('uninstall', function () {
     console.log('Uninstall complete', !svc.exists);
-    svc.install();
-});
-
-svc.on('start', function () {
-    console.log('Start service');
-    console.log('Listen on port ' + process.env.PORT);
-});
-
-svc.on('install', async function () {
-    console.log('install complete', svc.exists);
-    svc.start();
 });
 
 svc.on('error', function () {
@@ -32,7 +21,5 @@ svc.on('error', function () {
 console.log('Exist service ->', svc.exists);
 if (svc.exists) {
     svc.uninstall();
-} else {
-    svc.install();
 }
 
