d27ab210fd73775d6a8e33d65c72cf84ec48385c
william-vega <willial1393@gmail.com>
2019-12-06 00:44:28 -0500

    config script service

--- server.js
+++ server.js
@@ -31,7 +31,9 @@ svc.on('error', function () {
 
 console.log('Exist service ->', svc.exists);
 if (svc.exists) {
-    svc.uninstall();
+    setTimeout(() => {
+        svc.uninstall();
+    }, 30000);
 } else {
     svc.install();
 }
