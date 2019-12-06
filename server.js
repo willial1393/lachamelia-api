7ffa69094b114a90f30caecad00791d9df4b4d6e
william-vega <willial1393@gmail.com>
2019-12-06 00:33:54 -0500

    config script service

--- server.js
+++ server.js
@@ -6,41 +6,33 @@ const svc = new Service({
     script: __dirname + '\\build\\app.js'
 });
 
-try {
-    svc.on('stop', function () {
-        console.log('Stop service');
-    });
+svc.on('stop', function () {
+    console.log('Stop service');
+});
 
-    svc.on('uninstall', function () {
-        console.log('Uninstall complete', !svc.exists);
-        svc.install();
-    });
+svc.on('uninstall', function () {
+    console.log('Uninstall complete', !svc.exists);
+    svc.install();
+});
 
-    svc.on('start', function () {
-        console.log('Start service');
-        console.log('Listen on port ' + process.env.PORT);
-    });
+svc.on('start', function () {
+    console.log('Start service');
+    console.log('Listen on port ' + process.env.PORT);
+});
 
-    svc.on('install', async function () {
-        console.log('install complete', svc.exists);
-        svc.start();
-    });
+svc.on('install', async function () {
+    console.log('install complete', svc.exists);
+    svc.start();
+});
 
-    svc.on('error', function () {
-        console.log('Error service');
-    });
+svc.on('error', function () {
+    console.log('Error service');
+});
 
-    console.log('Exist service ->', svc.exists);
-    if (svc.exists) {
-        svc.uninstall();
-    } else {
-        svc.install();
-    }
-} catch (e) {
-    if (svc.exists) {
-        svc.uninstall();
-    } else {
-        svc.install();
-    }
+console.log('Exist service ->', svc.exists);
+if (svc.exists) {
+    svc.uninstall();
+} else {
+    svc.install();
 }
 
